02cf8fc62d20f6d0fde5081aacb045b037ed7929
Anu Tomy <anuatomy@gmail.com>
2017-12-02 12:17:03 -0500

    Examples of Constructors

---
+++ Constructors/Constructors/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+namespace Constructors
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+
+        public Customer(int id)
+            : this()           //first call the cunstructor with no parameter
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+            : this(id)         //first call the cunstructor that takes id parameter
+        {
+            //this.Id = id;
+            this.Name = name;
+        }
+
+    }
+}
