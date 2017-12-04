fd4e962e9fa16f86855b80f48e09f8cb60a90393
Anu Tomy <anuatomy@gmail.com>
2017-12-04 10:45:28 -0500

    Examples of property setting.

---
+++ Properties/Properties/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Person
+    {
+        public String Name{ get; set; } //Auto implemented property. 
+        public string Username{ get; set; }
+        public DateTime Birthdate { get; private set; } // the 'private set' accessor means it's readonly. Which means 'birthdate' cannot be changed
+
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+
+        
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }
+        }
+    }
+}
