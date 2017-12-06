7aa510019fe0af0d7c2fb81ea0455db26dc0a458
Anu Tomy <anuatomy@gmail.com>
2017-12-05 19:51:10 -0500

    Example of Constructors using 'base' keyword.

---
+++ Constructors_Section4/Constructors_Section4/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Constructors_Section4
+{
+    public class Car : Vehicle
+    {
+        public Car(string registrationNumber)
+            : base(registrationNumber)
+        {
+            Console.WriteLine("Car is being initialized. {0}" , registrationNumber);
+
+        }
+    }
+}
---
+++ Constructors_Section4/Constructors_Section4/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Constructors_Section4
+{
+    public class Vehicle
+    {
+        private readonly string _registrationNumber;
+
+        //public Vehicle()
+        //{
+        //    Console.WriteLine("Vehicle is being initialized.");
+        //}
+
+        public Vehicle(string registrationNumber)
+        {
+            this._registrationNumber = registrationNumber;
+            Console.WriteLine("Vehicle is being initialized. {0}", registrationNumber);
+        }
+    }
+}
