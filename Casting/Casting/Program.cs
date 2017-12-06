aa0842898ae5bfdbe51b683752e558b23959cfa3
Anu Tomy <anuatomy@gmail.com>
2017-12-06 10:49:39 -0500

    Example of Upcasting

---
+++ Casting/Casting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Upcasting
+            Text text = new Text();
+            Shape shape = text;      //Text and Shape objects pointing to(references to) same object in memory.  
+
+            //conversion from a derived class to a base class
+            text.Width = 200;
+            shape.Width = 100;    
+
+            Console.WriteLine(text.Width);  //100
+        }
+    }
+}
--- Casting/Casting/Properties/AssemblyInfo.cs
+++ Casting/Casting/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Section2_Exercise1_StopWatch")]
+[assembly: AssemblyTitle("Casting")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Section2_Exercise1_StopWatch")]
+[assembly: AssemblyProduct("Casting")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("784d3b86-e0ef-4f85-8902-29d4558691d5")]
+[assembly: Guid("a99d00ac-f4c4-41ad-b382-97cd66bf4a46")]
 
 // Version information for an assembly consists of the following four values:
 //
