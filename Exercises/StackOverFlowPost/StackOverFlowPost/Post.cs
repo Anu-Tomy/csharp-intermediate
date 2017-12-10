94db57c9d47b716e1d13fea436dc6a9285d0e710
Anu Tomy <anuatomy@gmail.com>
2017-12-09 20:21:48 -0500

    Complete the Exercise for checking current vote value of a post

---
+++ Exercises/StackOverFlowPost/StackOverFlowPost/Post.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+/*
+ * Design a class called Post. This class models a StackOverﬂow post. It should have properties for title, description and the date/time it was created. 
+ * We should be able to up-vote or down-vote a post. We should also be able to see the current vote value. 
+ * In the main method, create a post, up-vote and down-vote it a few times and then display the the current vote value. 
+ * 
+ * In this exercise, you will learn that a StackOverﬂow post should provide methods for up-voting and down-voting.   * You should not give the ability to set the Vote property from the outside, because otherwise, you may accidentally change the votes of a class to 0 or to a random number.  * And this is how we create bugs in our programs. The class should always protect its state and hide its implementation detail.    */
+
+namespace StackOverFlowPost
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; private set; }
+        public int Votes { get; set; }
+
+        public Post()
+        {
+           Created = DateTime.Now;
+        }
+
+
+        public Post(string title, string description)
+           : this()
+        {
+            this.Title = title;
+            this.Description = description;
+            this.Votes = 0;
+        }
+
+        //Methods
+        public int UpVote()
+        {
+            return ++Votes;
+        }
+        public int DownVote()
+        {
+            return --Votes;
+        }
+
+        public int CurrentVote()
+        {
+            if (Votes > 0)
+                return Votes;
+            else
+                return 0;
+        }
+
+
+    }
+}
---
+++ Exercises/StackOverFlowPost/StackOverFlowPost/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Design a class called Post. This class models a StackOverﬂow post. It should have properties for title, description and the date/time it was created. 
+ * We should be able to up-vote or down-vote a post. We should also be able to see the current vote value. 
+ * In the main method, create a post, up-vote and down-vote it a few times and then display the the current vote value. 
+ 
+   In this exercise, you will learn that a StackOverﬂow post should provide methods for up-voting and down-voting.     You should not give the ability to set the Vote property from the outside, because otherwise, you may accidentally change the votes of a class to 0 or to a random number.     And this is how we create bugs in our programs. The class should always protect its state and hide its implementation detail. 
+*/
+
+
+namespace StackOverFlowPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new Post("Test post","Test description");
+            Console.WriteLine("Post Title: " + post.Title);
+            Console.WriteLine("Description: " + post.Description);
+            Console.WriteLine("Time: " + post.Created);
+            Console.WriteLine("Current Votes: " + post.Votes);
+
+            Console.WriteLine("How many times do you want to Upvote/Downvote");
+            var count = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+
+                Console.WriteLine("Press 1 for up-vote and press 0 for down-vote");
+                var input = Convert.ToByte(Console.ReadLine());
+
+                if (input == 1)
+                {
+                    post.UpVote();
+                }
+                else
+                {
+                    post.DownVote();
+                }
+
+            }           
+             Console.WriteLine("Current Vote Value: " + post.CurrentVote());
+
+        }
+    }
+}
+
+
+
