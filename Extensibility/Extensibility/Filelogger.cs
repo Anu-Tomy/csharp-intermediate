237dc1e088fb214f4cd45ee1d90ed257c06d65f1
Anu Tomy <anuatomy@gmail.com>
2017-12-10 22:48:59 -0500

    Examples of Interfaces and Extensibility

---
+++ Extensibility/Extensibility/Filelogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Extensibility
+{
+    public class Filelogger : ILogger
+    {
+        private readonly string _path;
+
+        public Filelogger(string path)
+        {
+            _path = path;
+        }
+        public void LogError(string message)
+        {
+            using (var streamWriter = new StreamWriter(_path, true)) ;
+            {
+                Log(message, "ERROR");
+            }
+        }
+
+        public void LogInfo(string message)
+        {
+            Log(message, "INFO");
+        }
+
+        private void Log(string message, string messageType)
+        {
+            using (var streamWriter = new StreamWriter(_path, true))
+            {
+                streamWriter.WriteLine(messageType + ":" + message);
+            }
+        }
+    }
+}
---
+++ Extensibility/Extensibility/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigrator = new DbMigrator(new Filelogger("C:\\Test\\log.txt"));
+            dbMigrator.Migrate();
+        }
+    }
+}
